8d8ab8ccecdbdf61650af132f0b4b39676303449
Michelle1712 <87138771+Michelle1712@users.noreply.github.com>
2021-07-15 13:13:26 -0500

    Initial Commit

---
+++ Ask Me Anything Version 2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ask Me Anything Version 2
+//
+//  Created by Student26 on 7/15/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
